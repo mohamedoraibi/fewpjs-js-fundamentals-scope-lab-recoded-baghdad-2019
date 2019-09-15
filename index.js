fd8879ffe91c758ea515a1a7c68d5dda2f3a758b
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-15 19:13:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ function myAnimal() {
   return animal
 }
 
-function yourAnimal() {
+function yourAnimal(animalSend) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  animal = animalSend;
   return animal
 }
 
 function add2(n) {
-  let two = 2
-  return n + two
+
 
   // Feel free to move things around!
-  
+    let two = 2
+  return n + two
 }
